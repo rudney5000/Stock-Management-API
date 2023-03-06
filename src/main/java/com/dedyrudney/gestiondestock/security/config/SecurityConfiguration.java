5bfcc07cfdf6b6acc8ad6889646401d331b332fe
Your Name <you@example.com>
2023-03-06 03:07:33 +0300

    security controller

--- src/main/java/com/dedyrudney/gestiondestock/security/config/SecurityConfiguration.java
+++ src/main/java/com/dedyrudney/gestiondestock/security/config/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.dedyrudney.gestiondestock.security.config;
 
 import com.dedyrudney.gestiondestock.security.service.ApplicationUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,5 +29,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**/authenticate").permitAll()
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
---
+++ src/main/java/com/dedyrudney/gestiondestock/security/controller/AuthenticationController.java
@@ -0,0 +1,40 @@
+package com.dedyrudney.gestiondestock.security.controller;
+
+import com.dedyrudney.gestiondestock.security.dto.AuthenticationRequest;
+import com.dedyrudney.gestiondestock.security.dto.AuthenticationResponse;
+import com.dedyrudney.gestiondestock.security.service.ApplicationUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.dedyrudney.gestiondestock.utils.Constants.AUTHENTICATION_ENDPOINT;
+
+@RestController
+@RequestMapping(AUTHENTICATION_ENDPOINT)
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private ApplicationUserDetailsService applicationUserDetailsService;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticationResponse(@RequestBody AuthenticationRequest authenticationRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getLogin(),
+                        authenticationRequest.getPassword()
+                )
+        );
+
+        final UserDetails userDetails = applicationUserDetailsService.loadUserByUsername(authenticationRequest.getLogin());
+        return ResponseEntity.ok(AuthenticationResponse.builder().accessToken("stand_access_token").build());
+    }
+}
