f04b161b262cf618770ae994c5561ded31c2a87d
Your Name <you@example.com>
2023-03-13 00:12:26 +0300

    Photo Controller

--- src/main/java/com/dedyrudney/gestiondestock/controller/PhotoController.java
+++ src/main/java/com/dedyrudney/gestiondestock/controller/PhotoController.java
@@ -1,16 +1,26 @@
 package com.dedyrudney.gestiondestock.controller;
 
 import com.dedyrudney.gestiondestock.controller.api.PhotoApi;
+import com.dedyrudney.gestiondestock.service.strategy.StrategyPhotoContext;
 import com.flickr4java.flickr.FlickrException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
 public class PhotoController implements PhotoApi {
 
+    private StrategyPhotoContext strategyPhotoContext;
+
+    @Autowired
+    public PhotoController(StrategyPhotoContext strategyPhotoContext) {
+        this.strategyPhotoContext = strategyPhotoContext;
+    }
+
     @Override
-    public Object savePhoto(String context, Integer id, MultipartFile photo, String title) throws FlickrException {
-        return null;
+    public Object savePhoto(String context, Integer id, MultipartFile photo, String title) throws FlickrException, IOException {
+        return strategyPhotoContext.savedPhoto(context, id, photo.getInputStream(), title);
     }
 }
--- src/main/java/com/dedyrudney/gestiondestock/controller/api/PhotoApi.java
+++ src/main/java/com/dedyrudney/gestiondestock/controller/api/PhotoApi.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 import static com.dedyrudney.gestiondestock.utils.Constants.APP_ROOT;
 
 @Api("photos")
 public interface PhotoApi {
 
     @PostMapping(APP_ROOT + "/save/{id}/{title}/{context}")
-    Object savePhoto(@PathVariable("context") String context, @PathVariable("id") Integer id, @RequestPart("file") MultipartFile photo, @PathVariable("title") String title) throws FlickrException;
+    Object savePhoto(@PathVariable("context") String context, @PathVariable("id") Integer id, @RequestPart("file") MultipartFile photo, @PathVariable("title") String title) throws FlickrException, IOException;
 }
